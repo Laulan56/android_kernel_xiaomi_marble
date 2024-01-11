f7fa787bcc151fb7fc12c3b81fbcfd726a18bed9
Juhyung Park <qkrwngud825@gmail.com>
2024-01-12 02:02:29 +0900

    lazy_initcall: fix memory-leaks on info->hdr
    If userspace asks for an already-loaded module, __load_module() is
    skipped, causing a memory-leak on info->hdr.
    
    Fix it by reordering free_copy().
    
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>
    (cherry picked from commit 02a08267b18d27f5a3080a2090b96d244a410085)

--- kernel/lazy_initcall.c
+++ kernel/lazy_initcall.c
@@ -353,7 +353,6 @@ static noinline int __init __load_module(struct load_info *info, const char __us
 	load_modname(info->name, uargs, NORMAL);
 
 err:
-	free_copy(info);
 	return err;
 }
 
@@ -387,6 +386,7 @@ static int __ref load_module(struct load_info *info, const char __user *uargs,
 	}
 
 out:
+	free_copy(info);
 	mutex_unlock(&lazy_initcall_mutex);
 	return ret;
 }
