0652dc95721a256d0e8f932d8881f871d24e387e
Jens Reidel <adrian@travitia.xyz>
2024-04-26 17:03:27 +0200

    camera-kernel: Support s12b cam_vana1 voltage override
    Change-Id: Ida96ed28b74b8161aab727f80e644a42b1c7990a
    Signed-off-by: Jens Reidel <adrian@travitia.xyz>

--- techpack/camera/drivers/cam_utils/cam_soc_util.c
+++ techpack/camera/drivers/cam_utils/cam_soc_util.c
@@ -2538,6 +2538,9 @@ int cam_soc_util_regulator_disable(struct regulator *rgltr,
 	return rc;
 }
 
+#ifdef CONFIG_S12B
+static uint32_t cam_power_vana1_volt = 1300000;
+#endif
 
 int cam_soc_util_regulator_enable(struct regulator *rgltr,
 	const char *rgltr_name,
@@ -2554,6 +2557,12 @@ int cam_soc_util_regulator_enable(struct regulator *rgltr,
 	if (regulator_count_voltages(rgltr) > 0) {
 		CAM_DBG(CAM_UTIL, "voltage min=%d, max=%d",
 			rgltr_min_volt, rgltr_max_volt);
+
+#ifdef CONFIG_S12B
+		if (!strcmp(rgltr_name, "cam_vana1")) {
+			rgltr_min_volt = cam_power_vana1_volt;
+		}
+#endif
 
 		rc = regulator_set_voltage(
 			rgltr, rgltr_min_volt, rgltr_max_volt);
